f1e819227c00736790df9323fe4836dcea0148e4
Konstantin <konstantinagaf@gmail.com>
2019-02-04 00:01:33 +0500

    Add test js, css, html.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,11 @@ INSERT INTO app_role (role_id, role_name) VALUES
 (2,'USER');
 
 INSERT INTO app_user (user_id, enable, encrypted_password, username) VALUES
- (1,1,'$2a$10$nx/Gyaau2569/Jb6OlB5MuHKl4Om/EcR.ArRDcStJdopp609wlhhK', 'dbadmin');
+ (1,1,'$2a$10$nx/Gyaau2569/Jb6OlB5MuHKl4Om/EcR.ArRDcStJdopp609wlhhK', 'dbadmin'),
+ (2,1,'$2a$10$nx/Gyaau2569/Jb6OlB5MuHKl4Om/EcR.ArRDcStJdopp609wlhhK', 'dbuser');
 
  INSERT INTO user_role (user_role_id, role_id, user_id) values
- (1,1,1);
+ (1,1,1),
+ (2,2,1),
+ (3,2,2);
 
